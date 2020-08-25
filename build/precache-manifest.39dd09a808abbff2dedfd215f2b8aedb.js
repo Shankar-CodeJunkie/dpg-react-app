ac2936999aedd68d2c1eadab446eed20e7d06992
Sovik Roy <sovik.roy@in.ibm.com>
2020-08-25 12:23:55 +0530

    updated build folder

---
+++ build/precache-manifest.39dd09a808abbff2dedfd215f2b8aedb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bf8285503d247568dc8dd54c49fe11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10b99928b36f967c9dbf",
+    "url": "/static/css/main.98ee2b99.chunk.css"
+  },
+  {
+    "revision": "1bdf3d5362553acef810",
+    "url": "/static/js/2.ed4c38e9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ed4c38e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b99928b36f967c9dbf",
+    "url": "/static/js/main.75a43368.chunk.js"
+  },
+  {
+    "revision": "3c7beecf55bdb387b928",
+    "url": "/static/js/runtime-main.d3f8a8db.js"
+  }
+]);
